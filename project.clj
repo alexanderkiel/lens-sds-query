9c63795be2d8d01c20750223a346b53299deec17
Alexander Kiel <alexanderkiel@gmx.net>
2016-03-30 13:29:39 +0200

    Release Version 0.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lens-sds-query "0.4"
+(defproject lens-sds-query "0.5"
   :description "Lens Study Data Store Query Service"
   :url "https://github.com/alexanderkiel/lens-sds-query"
   :license {:name "Eclipse Public License"
