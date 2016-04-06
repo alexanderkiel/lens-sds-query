a9c12ff7777239ea2bcd11341b9e4ce3643e85c6
Alexander Kiel <alexanderkiel@gmx.net>
2016-04-06 16:38:08 +0200

    Log Start/Stop of Server

--- src/lens/server.clj
+++ src/lens/server.clj
@@ -2,19 +2,25 @@
   (:require [clojure.core.cache :as cache]
             [com.stuartsierra.component :refer [Lifecycle]]
             [lens.app :refer [app]]
+            [lens.logging :as log]
             [org.httpkit.server :refer [run-server]]))
 
 (defn ttl-cache [ttl]
   (cache/ttl-cache-factory {} :ttl ttl))
 
+(defn- info [m]
+  (log/info (assoc m :component "Server")))
+
 (defrecord Server [port thread database stop-fn]
   Lifecycle
   (start [server]
+    (info {:action :start :port port})
     (let [auth-cache (atom (ttl-cache 60000))
           opts (assoc server :conn (:conn database) :auth-cache auth-cache)
           handler (app opts)]
       (assoc server :stop-fn (run-server handler opts))))
   (stop [server]
+    (info {:action :stop})
     (stop-fn)
     (assoc server :stop-fn nil)))
 
